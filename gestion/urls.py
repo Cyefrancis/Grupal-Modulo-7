1f43defd909cee316043dfbe4502084acae784cb
Francisco Navarrete <frncisco1243@gmail.com>
2024-06-17 00:55:54 -0400

    grupal 2 teminada

--- gestion/urls.py
+++ gestion/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('productos/', views.lista_productos, name='lista_productos'),
     path('productos/agregar/', views.agregar_producto, name='agregar_producto'),
     path('pedidos/', views.lista_pedidos, name='lista_pedidos'),
+    path('tomar_pedido/', views.tomar_pedido, name='tomar_pedido'),
+    path('guardar_pedido/', views.guardar_pedido, name='guardar_pedido'), 
     path('pedidos/<int:pedido_id>/', views.detalle_pedido, name='detalle_pedido'),
 ]
--- gestion/views.py
+++ gestion/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib import messages
 from .models import Producto, Pedido, DetallePedido
 from .forms import ProductoForm
+import decimal
 # Create your views here.
 
 def lista_productos(request):
@@ -29,3 +31,39 @@ def detalle_pedido(request, pedido_id):
     pedido = Pedido.objects.get(id=pedido_id)
     detalles = DetallePedido.objects.filter(pedido=pedido)
     return render(request, 'gestion/detalle_pedido.html', {'pedido': pedido, 'detalles': detalles})
+
+def tomar_pedido(request):
+    productos = Producto.objects.all()
+    return render(request, 'gestion/tomar_pedido.html', {'productos': productos})
+
+def guardar_pedido(request):
+    if request.method == 'POST':
+        cliente = request.POST.get('cliente')
+        direccion = request.POST.get('direccion')
+        productos = request.POST.getlist('productos')
+        cantidades = request.POST.getlist('cantidades')
+        precios_unitarios = request.POST.getlist('precios_unitarios')
+
+        if not cliente or not direccion or not productos or not cantidades or not precios_unitarios:
+            messages.error(request, "Todos los campos son obligatorios.")
+            return redirect('tomar_pedido')
+
+        pedido = Pedido(cliente=cliente, direccion_entrega=direccion)
+        pedido.save()
+
+        for producto_id, cantidad, precio_unitario in zip(productos, cantidades, precios_unitarios):
+            producto = Producto.objects.get(id=producto_id)
+            detalle = DetallePedido(
+                pedido=pedido,
+                producto=producto,
+                cantidad=int(cantidad),
+                precio_unitario=decimal.Decimal(precio_unitario)
+            )
+            detalle.save()
+
+        messages.success(request, "Pedido guardado exitosamente.")
+        return redirect('lista_pedidos')
+
+    else:
+        messages.error(request, "Método no permitido.")
+        return redirect('tomar_pedido')
